03b0fbf9150dca1d301466b0dd0af4729f1992e4
doomsdayrs <rahim13657>
2020-01-27 21:44:28 -0500

    - Minor patch to NovelFull.lua

--- core/com/github/doomsdayrs/api/shosetsu/services/core/Test.kt
+++ core/com/github/doomsdayrs/api/shosetsu/services/core/Test.kt
@@ -96,27 +96,29 @@ private object Test {
 
             // Latest
             java.util.concurrent.TimeUnit.SECONDS.sleep(1)
-            val list = luaFormatter.parseLatest(docFromURL(luaFormatter.getLatestURL(1)))
-            println(list)
+            //  val list = luaFormatter.parseLatest(docFromURL(luaFormatter.getLatestURL(1)))
+            //           println(list)
             println()
 
+
+            println(luaFormatter.getSearchString("I alone"))
             // Search
             java.util.concurrent.TimeUnit.SECONDS.sleep(1)
             @Suppress("SpellCheckingInspection")
-            println(luaFormatter.parseSearch(docFromURL(luaFormatter.getSearchString("reinca"))))
+            //    println(luaFormatter.parseSearch(docFromURL(luaFormatter.getSearchString("reinca"))))
             println()
 
             // Novel
             java.util.concurrent.TimeUnit.SECONDS.sleep(1)
-            val novel = luaFormatter.parseNovel(docFromURL(luaFormatter.novelPageCombiner(list[0].link, 1)), 1)
-            println(novel)
+            //      val novel = luaFormatter.parseNovel(docFromURL(luaFormatter.novelPageCombiner(list[0].link, 1)), 1)
+            //   println(novel)
 
             // Parse novel passage
             java.util.concurrent.TimeUnit.SECONDS.sleep(1)
             println(run {
-                val it = luaFormatter.getNovelPassage(docFromURL(novel.novelChapters[0].link))
-                if (it.length < 25) "Result: $it"
-                else "${it.length} chars long result: ${it.take(10)} [...] ${it.takeLast(10)}"
+                //           val it = luaFormatter.getNovelPassage(docFromURL(novel.novelChapters[0].link))
+                //             if (it.length < 25) "Result: $it"
+                //            else "${it.length} chars long result: ${it.take(10)} [...] ${it.takeLast(10)}"
             })
             println()
 
