ed260288fa14ff4972d983da38bf8ab1f67450f1
TechnoJo4 <technojo4@gmail.com>
2020-10-06 19:55:24 -0400

    add support for tables for more base functions & document them

--- src/main/resources/filter.lua
+++ src/main/resources/filter.lua
@@ -1,10 +1,23 @@
+-- o is a table or java object with size and get methods (usually Elements or ArrayList)
+-- f is a function mapping every value from that table or object to the boolean that defines whether it is included in the resulting array
 local o, f = ...
-local t, j = {}, 1
-for i=0, o:size()-1 do
-    local v = o:get(i)
-    if f(v) then
-        t[j] = v
-        j = j + 1
+local t, j = {}, 0
+
+if type(o) == "table" then
+    for k,v in pairs(o) do
+        if f(v, k) then
+            j = j + 1
+            t[k] = v
+        end
+    end
+else
+    for i=0, o:size()-1 do
+        local v = o:get(i)
+        if f(v, i) then
+            j = j + 1
+            t[j] = v
+        end
     end
 end
+
 return t
--- src/main/resources/flatten.lua
+++ src/main/resources/flatten.lua
@@ -1,10 +1,12 @@
+-- Flattens a table of tables into a single table
 local t = ...
-local n = {}
-local i = 1
+local n, i = {}, 1
+
 for _,u in pairs(t) do
     for _,v in pairs(u) do
         n[i] = v
         i = i + 1
     end
 end
+
 return n
