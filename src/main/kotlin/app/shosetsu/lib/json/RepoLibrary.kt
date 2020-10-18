30c3085e18db4ab57ba94e0f371baeec37089431
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2020-10-17 22:13:42 -0400

    Parsed index (#8)
    * - Removed ReadNovelForLife.lua, Ded site
    - Removed SaikaiScan.lua search function
    - Malicious code fixing
    - Renamed LuaFormatter.kt to LuaExtension.kt
    - Created subdirectories for specific script types
    - Renamed ShosetsuLib.kt to ShosetsuLuaLib.kt
    - Moved LuaExtension.kt and ShosetsuLuaLib.kt to lua sub directory
    - type alias for Formatter.kt to IExtension.kt
    
    * - isolated lua to the lua sub folder
    
    * - fixed unresolved reference in Test.kt
    
    * add support for tables for more base functions & document them
    
    * - merged documentation + fixed funcs
    - added metadata function to IExtension.kt
    
    * stop using top-level for lua functions. add Novel, NovelChapter & NovelInfo table constructors.
    
    * fix
    
    * - merged technoj4s dev
    - removed old.kt
    - tested extensions
    
    * - Added Version.kt
    - removed formatters.json
    - Modified extensions to use new headers
    
    * - Added names.kt with json keys
    - LuaExtension.kt now uses names.kt
    - index.json with updated keys
    
    * - added build to .gitignore
    
    * - fixed unterminated string errors in json for NovelFull.lua & BestLightNovel.lua
    - Test.kt now has colors
    - Test.kt now increments through novels to find one with chapters
    - LuaExtension.kt now has new checks for missing parameters
    
    * - using new shosetsuorg luaj repository
    
    * - kotlin-lib now handles RepoIndex.kt and its parsing
    
    Co-authored-by: TechnoJo4 <technojo4@gmail.com>

---
+++ src/main/kotlin/app/shosetsu/lib/json/RepoLibrary.kt
@@ -0,0 +1,11 @@
+package app.shosetsu.lib.json
+
+import app.shosetsu.lib.Version
+
+/**
+ * shosetsu-kotlin-lib
+ * 17 / 10 / 2020
+ *
+ * Represents a library listed in `libraries` within the repository index
+ */
+data class RepoLibrary internal constructor (val name: String, val version: Version)
