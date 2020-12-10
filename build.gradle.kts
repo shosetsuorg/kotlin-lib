8211f929c4eba6cf0cdb3f7a56808e2ff608864d
doomsdayrs <doomsdayrs@gmail.com>
2020-12-10 08:15:49 -0500

    - fixed gradle ?
    Having issues with gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "1.0.0"
 description = "Kotlin library for shosetsu"
 
 plugins {
-	kotlin("jvm") version "1.4.10"
+	kotlin("jvm") version "1.4.20"
 	id("org.jetbrains.dokka") version "0.10.0"
 	maven
 }
@@ -32,12 +32,13 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 	implementation("org.jsoup:jsoup:1.12.1")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.20")
 	implementation("org.luaj:luaj-jse:3.0.1")
 	implementation("com.beust:klaxon:5.0.1")
 	implementation("com.squareup.okhttp3:okhttp:4.2.1")
 	implementation("com.google.guava:guava:30.0-jre")
 	testImplementation("junit:junit:4.12")
+
 //	implementation("org.jetbrains.kolin:kotlin-test:v1.3.61")
 }
 
