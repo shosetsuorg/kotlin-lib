f13f6501f5a26a18f114c05b7c012b731b7aaba8
doomsdayrs <doomsdayrs@gmail.com>
2021-06-04 01:14:04 -0400

    Updated gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,12 @@ version = "1.0.0"
 description = "Kotlin library for shosetsu"
 
 plugins {
-	kotlin("jvm") version "1.5.0"
+	kotlin("jvm") version "1.5.10"
 	id("org.jetbrains.dokka") version "1.4.32"
-	kotlin("plugin.serialization") version "1.5.0"
+	kotlin("plugin.serialization") version "1.5.10"
 	maven
 }
 tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
-
 
 
 val dokkaJar by tasks.creating(Jar::class) {
@@ -28,19 +27,18 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("org.jsoup:jsoup:1.12.1")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.0")
+	implementation("org.jsoup:jsoup:1.13.1")
+	implementation(kotlin("stdlib-jdk8"))
 	dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.32")
 
 	// java only
 	implementation("org.luaj:luaj-jse:3.0.1")
-	implementation("com.squareup.okhttp3:okhttp:4.2.1")
-	implementation("com.google.guava:guava:30.0-jre")
-	implementation("net.java.dev.jna:jna:4.2.2")
+	implementation("com.squareup.okhttp3:okhttp:4.9.1")
+	implementation("com.google.guava:guava:30.1.1-jre")
+	implementation("net.java.dev.jna:jna:5.8.0") // for KTS
 
 	// Cross platform confirmed
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-	testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 
 	implementation(kotlin("reflect"))
 	implementation(kotlin("script-runtime"))
@@ -49,6 +47,7 @@ dependencies {
 	implementation(kotlin("scripting-compiler-embeddable"))
 	implementation(kotlin("script-util"))
 
+	testImplementation(kotlin("test"))
 }
 
 val compileTestKotlin: KotlinCompile by tasks
