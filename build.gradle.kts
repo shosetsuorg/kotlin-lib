612b85c57197a392370fd97147b606d0f9a97be9
doomsdayrs <doomsdayrs@gmail.com>
2021-06-10 11:25:43 -0400

    Refactored dependencies. Test only dependencies isolated

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,29 +26,34 @@ repositories {
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
-	implementation("org.jsoup:jsoup:1.13.1")
-	implementation(kotlin("stdlib-jdk8"))
 	dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.32")
 
+	// ### Core Libraries
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+
 	// java only
+	implementation("org.jsoup:jsoup:1.13.1")
 	implementation("org.luaj:luaj-jse:3.0.1")
 	implementation("com.squareup.okhttp3:okhttp:4.9.1")
 	implementation("com.google.guava:guava:30.1.1-jre")
-	implementation("net.java.dev.jna:jna:5.8.0") // for KTS
 
-	// Cross platform confirmed
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 
-	implementation(kotlin("reflect"))
-	implementation(kotlin("script-runtime"))
-	implementation(kotlin("script-util"))
-	implementation(kotlin("compiler-embeddable"))
-	implementation(kotlin("scripting-compiler-embeddable"))
-	implementation(kotlin("script-util"))
+	// ### Testing Libraries
+	testImplementation(kotlin("stdlib"))
+	testImplementation(kotlin("stdlib-jdk8"))
+
+	testImplementation("net.java.dev.jna:jna:5.8.0") // for KTS
+
+	testImplementation(kotlin("reflect"))
+	testImplementation(kotlin("script-runtime"))
+	testImplementation(kotlin("script-util"))
+	testImplementation(kotlin("compiler-embeddable"))
+	testImplementation(kotlin("scripting-compiler-embeddable"))
+	testImplementation(kotlin("script-util"))
 
 	testImplementation(kotlin("test"))
 }
+
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
