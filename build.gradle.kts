a8fd3cb90400cdaf76eec10deefdfeabe1d4686f
doomsdayrs <doomsdayrs@gmail.com>
2021-11-02 23:31:05 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ version = "1.0.0"
 description = "Kotlin library for shosetsu"
 
 plugins {
-	kotlin("jvm") version "1.5.10"
-	id("org.jetbrains.dokka") version "1.4.32"
-	kotlin("plugin.serialization") version "1.5.10"
+	kotlin("jvm") version "1.5.31"
+	id("org.jetbrains.dokka") version "1.5.31"
+	kotlin("plugin.serialization") version "1.5.31"
 	maven
 }
 tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
@@ -26,23 +26,23 @@ repositories {
 }
 
 dependencies {
-	dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.32")
+	dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.5.31")
 
 	// ### Core Libraries
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
 	// java only
-	implementation("org.jsoup:jsoup:1.13.1")
+	implementation("org.jsoup:jsoup:1.14.3")
 	implementation("org.luaj:luaj-jse:3.0.1")
-	implementation("com.squareup.okhttp3:okhttp:4.9.1")
-	implementation("com.google.guava:guava:30.1.1-jre")
+	implementation("com.squareup.okhttp3:okhttp:4.9.2")
+	implementation("com.google.guava:guava:31.0.1-jre")
 
 
 	// ### Testing Libraries
 	testImplementation(kotlin("stdlib"))
 	testImplementation(kotlin("stdlib-jdk8"))
 
-	testImplementation("net.java.dev.jna:jna:5.8.0") // for KTS
+	testImplementation("net.java.dev.jna:jna:5.9.0") // for KTS
 
 	testImplementation(kotlin("reflect"))
 	testImplementation(kotlin("script-runtime"))
