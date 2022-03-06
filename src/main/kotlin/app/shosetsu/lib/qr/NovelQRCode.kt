38cd4ba71a3a492b63a4f0b064e28e3787798954
doomsdayrs <doomsdayrs@gmail.com>
2022-03-06 15:17:32 -0500

    Implement QRCode data classes

---
+++ src/main/kotlin/app/shosetsu/lib/qr/NovelQRCode.kt
@@ -0,0 +1,9 @@
+package app.shosetsu.lib.qr
+
+/**
+ * 06 / 03 / 2022
+ */
+data class NovelQRCode(
+	val url: String,
+	val extensionQRCode: ExtensionQRCode
+)
---
+++ src/main/kotlin/app/shosetsu/lib/qr/StyleQRCode.kt
@@ -0,0 +1,9 @@
+package app.shosetsu.lib.qr
+
+/**
+ * 06 / 03 / 2022
+ */
+data class StyleQRCode(
+	val id: Int,
+	val repo: RepositoryQRCode
+)
